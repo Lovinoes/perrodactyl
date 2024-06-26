aeddbd057d6d76f6bf7efc11240675830db7b99d
Lovinoes <lolprobs12@gmail.com>
2024-06-26 19:43:55 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,17 @@
+cd /var/www/pterodactyl
+php artisan down
+
+curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
+chmod -R 755 storage/* bootstrap/cache
+yes | composer install --no-dev --optimize-autoloader
+php artisan view:clear
+php artisan config:clear
+php artisan migrate --seed --force
+chown -R nginx:nginx /var/www/pterodactyl/*
+
+php artisan queue:restart
+php artisan up
+
+echo ""
+echo "updated..."
+echo ""
