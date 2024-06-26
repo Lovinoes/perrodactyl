e50d758da3c1a9d9982066d7a31bb50b81e4bf05
Lovinoes <lolprobs12@gmail.com>
2024-06-26 19:57:46 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,21 +1,22 @@
-cd /var/www/pterodactyl
-php artisan down
+#!/bin/bash
 
+cd /var/www/pterodactyl
+export COMPOSER_ALLOW_SUPERUSER=1
+
+php artisan down
 rm -rf resources/ public/ node_modules/
+
 curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
 chmod -R 755 storage/* bootstrap/cache
-
-export COMPOSER_ALLOW_SUPERUSER=1
 composer install --no-dev --optimize-autoloader --no-plugins
-
 php artisan view:clear
 php artisan config:clear
 php artisan migrate --seed --force
 chown -R nginx:nginx /var/www/pterodactyl/*
-
 php artisan queue:restart
+
 php artisan up
 
 echo ""
-echo "updated..."
+echo "Updated Pterodactyl panel successfully."
 echo ""
