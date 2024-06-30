068c3bbcb954045220024aecd748dce237f0838d
Lovinoes <lolprobs12@gmail.com>
2024-06-30 16:09:48 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@
 cd /var/www/pterodactyl
 export COMPOSER_ALLOW_SUPERUSER=1
 
+# start
 php artisan down
 rm -rf resources/ public/ node_modules/
 
@@ -12,11 +13,17 @@ composer install --no-dev --optimize-autoloader --no-plugins
 php artisan view:clear
 php artisan config:clear
 php artisan migrate --seed --force
-chown -R nginx:nginx /var/www/pterodactyl/*
+
+# Change ownership of the files
+if [[ -f /etc/redhat-release ]]; then
+    chown -R nginx:nginx /var/www/pterodactyl/* || { echo "Failed to change file ownership"; exit 1; }
+elif [[ -f /etc/debian_version ]]; then
+    chown -R www-data:www-data /var/www/pterodactyl/* || { echo "Failed to change file ownership"; exit 1; }
+fi
+
+# end
 php artisan queue:restart
-
 php artisan up
-
 echo ""
 echo "Updated Pterodactyl panel successfully."
 echo ""
